477747225387890d6f8d4f1060e7e840bf699e7d
Denis <130212414+neron776@users.noreply.github.com>
2024-05-05 17:21:50 +0300

    Add files via upload

---
+++ app/common/commands_list.py
@@ -0,0 +1,6 @@
+from aiogram.types import BotCommand
+
+private = [
+    BotCommand(command='start', description='Показать приветствие'),
+    BotCommand(command='show_time', description='Показать текущее время'),
+]
